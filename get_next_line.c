abb67d1d100c562512d8c61a7ae1330ddc4e7c52
Maxens Gagne <mgagne@z4r6p5.42lyon.fr>
2022-11-22 12:44:01 +0100

    gitignore + lib

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,13 @@
 /*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 16:45:25 by mgagne            #+#    #+#             */
-/*   Updated: 2022/11/22 12:28:40 by mgagne           ###   ########.fr       */
+/*   Updated: 2022/11/22 12:42:38 by mgagne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *get_next_line(int fd)
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
 {
 
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,15 @@
 /*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 16:42:07 by mgagne            #+#    #+#             */
-/*   Updated: 2022/11/17 16:42:10 by mgagne           ###   ########.fr       */
+/*   Updated: 2022/11/22 12:42:41 by mgagne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
